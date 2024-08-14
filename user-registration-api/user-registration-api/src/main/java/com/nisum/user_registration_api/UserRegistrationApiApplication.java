c2203826fe361fb4562b7587c4074b5225096b7a
sebastian picardo <spicardo@pgagroup.cl>
2024-08-14 11:49:44 -0400

    inicio de proyecto

---
+++ user-registration-api/user-registration-api/src/main/java/com/nisum/user_registration_api/UserRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.user_registration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationApiApplication.class, args);
+	}
+
+}
